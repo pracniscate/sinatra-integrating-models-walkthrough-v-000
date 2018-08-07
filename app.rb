bd825a57187474f444f31caf5b0f0041aa254754
Lorena <lorena.giddens@outlook.com>
2018-08-07 15:51:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
-  get '/' do
+  get '/' do # renders the index.rb page
     erb :index
   end
 
-  post '/' do
+  post '/' do # receives the form data through params & renders the results page
     text_from_user = params[:user_text]
 
     erb :results
